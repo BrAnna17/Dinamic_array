a5a47ecae2ac5f16251f06459ba27064bb80e8d0
BrAnna17 <34571335+BrAnna17@users.noreply.github.com>
2017-12-15 23:17:20 +0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -12,11 +12,29 @@ Creation of dynamic array using pointers*
 
 using namespace std;
 
-struct NewMass
+struct NewMass // Объявляем структуру. Поле  k -  ключи,  v - значения
 {
 	int * k, * v;
 };
  
+NewMass* sort(int* keys, int* values, NewMass* third, int len) //Функция, реализурующая сортировку нового массива, возвращает
+{						              // указатель на измененную структуру
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = 0; j < len - 1; j++)
+		{
+			if (*third[j + 1].k <*third[j].k)
+			{
+				NewMass tmp = third[j + 1];
+				third[j + 1] = third[j];
+				third[j] = tmp;
+			}
+		}
+	}
+
+	return (third);
+}
+
  
 
 int main()
@@ -24,22 +42,21 @@ int main()
 	int len = 0; //Начальная длина
 	cout << " Write number of keys $ values " << endl;
 	cin >> len;
-	cout << " Your array is: " << endl;
-
-	int * keys = (int*)malloc(len*sizeof(int)); 
+	int * keys = (int*)malloc(len*sizeof(int)); //Выделяем динамическую память для ключей и значений
 	int * values = (int*)malloc(len*sizeof(int));
 	struct NewMass * third = (NewMass*)malloc(len*sizeof(NewMass));
-	if (keys == NULL) return (1);
+	if (keys == NULL) return (1); 
 	if (values == NULL) return (1);
 
-	for (int i = 0; i < len; i++) //заполнение массива
+	for (int i = 0; i < len; i++) //Заполнение массива
 	{
 		keys[i] = rand() % 50;
 		values[i] = rand() % 50;
 		third[i].k = &keys[i];
 		third[i].v = &values[i];
 	}
-	for (int i = 0; i < len; i++) //вывод массива
+	cout << " Your array is: " << endl; 
+	for (int i = 0; i < len; i++) // Печатаем  массив
 		cout<<*third[i].k<<" "<<*third[i].v<<endl;
 
 	cout<<" Lets add an element "<<endl;
@@ -47,12 +64,12 @@ int main()
 	len++;
 	cout<<" Enter key: ";
 	cin>>input;
-	keys = (int*)realloc(keys, len * sizeof(int));
-	keys[len - 1] = input;
+	keys = (int*)realloc(keys, len * sizeof(int)); //Изменяем размер динамического массива ключей
+	keys[len - 1] = input; // Добавляем ключ
 	cout<<" Enter value: ";
 	cin>>input;
-	values = (int*)realloc(values, len * sizeof(int));
-	values[len - 1] = input;
+	values = (int*)realloc(values, len * sizeof(int)); //Изменяем размер динамического массива значений
+	values[len - 1] = input; // Добавляем значение
 
 	third = (NewMass*)realloc(third, len * sizeof(NewMass));
 	for (int i = 0; i < len; i++) //заполнение массива с новым элементом
@@ -61,28 +78,17 @@ int main()
 		third[i].v = &values[i];
 	}
 
-	for (int i = 0; i < len; i++) //вывод
+	for (int i = 0; i < len; i++) //вывод массива с новым элементом в конце (просто добавили )
 		cout<<*third[i].k<<" "<<*third[i].v<<endl;
 
-	cout << " Lets sort this array : " << endl ; //Сортировка
-	for (int i = 0; i < len - 1; i++)
-	{
-		for (int j = 0; j < len - 1; j++)
-		{
-			if (*third[j + 1].k < *third[j].k)
-			{
-			        struct NewMass tmp = third[j + 1];
-				third[j + 1] = third[j];
-				third[j] = tmp;
-			}
-		}
-	}
-
-	for (int i = 0; i < len; i++) //вывод
+	cout << " Lets sort this array : " << endl ;
+	
+	third = sort(keys, values, third, len);// Возвращаем указатель на измененную структуру с уже отсортированными элементами
+	for (int i = 0; i < len; i++) //выводим итог
 		cout<<*third[i].k<<" "<<*third[i].v<<endl;
 
 
-	free(keys); free(values); free(third); 
+	free(keys); free(values); free(third); // Освобождаем память
 	system("pause");
 	return 0;
 }
